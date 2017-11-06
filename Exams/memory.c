2bf91c76fe5ff3a1912617d84c5b9f91d092ced7
Matteo Di Pirro <matteodipirro@gmail.com>
2017-11-06 17:01:43 +0100

    Add function documentation

--- Exams/memory.c
+++ Exams/memory.c
@@ -13,14 +13,19 @@ static int spot;
 static struct node* unallocated;
 static struct node* allocated;
 
+/**
+ * Add a new chunk in the `allocated` list. The insertion respects
+ * the address ordering.
+ * @param chunk The chunk to insert
+ */ 
 void allocate_respecting_addresses(struct node* chunk) {
     struct node *node = allocated, *prev = NULL;
     if (node) {
-        while (node && node < chunk) {
+        while (node && node < chunk) { // look for the chunk immediately after the parameter
             prev = node;
             node = node -> next;
         }
-        if (node) {
+        if (node) { // chunk has to be inserted before the end
             chunk -> next = node;
             chunk -> prev = node -> prev;
             if (node -> prev) { // insert chunk
@@ -29,7 +34,7 @@ void allocate_respecting_addresses(struct node* chunk) {
                 allocated = chunk;
             }
             node -> prev = chunk;
-        } else {
+        } else { // chunk has to be inserted at the end
             prev -> next = chunk;
             chunk -> prev = prev;
         }
@@ -38,6 +43,14 @@ void allocate_respecting_addresses(struct node* chunk) {
     }
 }
 
+/**
+ * Take an eligible free chunk and make it busy according to the
+ * memory request (meaning the entire chunk can be made busy or 
+ * only a part of that).
+ * @param chunk The chunk to be made busy
+ * @param size The requested memory for allocation
+ * @return 0 If everything went good, 1 otherwise
+ */ 
 int reduce_free_chunk(struct node* chunk, size_t size) {
     if (size == chunk -> size) { // exact fit
         if (spot - chunk -> size < 0) {
@@ -76,6 +89,11 @@ int reduce_free_chunk(struct node* chunk, size_t size) {
     return 0;
 }
 
+/**
+ * Allocate size bytes.
+ * @param size The memory to allocate.
+ * @return A pointer to the allocated memory
+ */ 
 void* my_malloc(size_t size) {
     short found = 0, status = 1;
     struct node* chunk = unallocated;
@@ -91,6 +109,11 @@ void* my_malloc(size_t size) {
     return (chunk && !status) ? chunk + sizeof(struct node) : NULL;
 }
 
+/**
+ * Insert a chunk in `unallocated` respecting the address ordering.
+ * Merge if necessary.
+ * @param chunk The chunk to be inserted in unallocated
+ */ 
 void free_respecting_addresses(struct node* chunk) {
     struct node *node = unallocated, *prev = NULL;
     short left_merged = 0, right_merged = 0;
@@ -154,6 +177,11 @@ void free_respecting_addresses(struct node* chunk) {
     }
 }
 
+/**
+ * Find a busy chunk in the allocated list.
+ * @param A pointer to the chunk to be found.
+ * @return 0 if chunk not found, 1 otherwise
+ */ 
 int find_busy_chunk(struct node* chunk) {
     if (!unallocated) {
         return 0;
@@ -167,6 +195,10 @@ int find_busy_chunk(struct node* chunk) {
     return found;
 }
 
+/**
+ * Free a memory area allocated with my_malloc
+ * @param ptr A pointer to the memory area to free
+ */ 
 void my_free(void* ptr) {
     struct node* chunk = ((struct node*)ptr - sizeof(struct node)); // get the corresponding chunk
     if (find_busy_chunk(chunk)) {
